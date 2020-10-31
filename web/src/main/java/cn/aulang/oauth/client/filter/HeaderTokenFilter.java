581375d15a7396b3a2385408a8b2ea6156594154
Aulang <aulang@qq.com>
2020-11-01 01:15:52 +0800

    nginx跨域

--- web/src/main/java/cn/aulang/oauth/client/filter/HeaderTokenFilter.java
+++ web/src/main/java/cn/aulang/oauth/client/filter/HeaderTokenFilter.java
@@ -8,8 +8,6 @@ import cn.aulang.oauth.client.user.UserHolder;
 import cn.aulang.office.web.common.Constants;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,8 +16,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-import static org.springframework.web.cors.CorsConfiguration.ALL;
 
 /**
  * @author Aulang
@@ -32,23 +28,9 @@ public class HeaderTokenFilter extends OncePerRequestFilter {
     private OAuthTemplate template;
     private UserDetailsService userService;
 
-    private void allowCORS(HttpServletResponse response) {
-        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ALL);
-        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, ALL);
-        response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, ALL);
-    }
-
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        allowCORS(response);
-
-        String method = request.getMethod();
-        if (HttpMethod.OPTIONS.name().equalsIgnoreCase(method)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         String token = request.getParameter(Constants.ACCESS_TOKEN);
         String authorization = request.getHeader(Constants.AUTHORIZATION);
 
